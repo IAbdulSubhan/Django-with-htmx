3c40baa345fb60d07d96eee77fd632e43501e6ff
IAbdulSubhan <subhankhalidofficial@gmail.com>
2025-07-10 23:01:58 +0500

    Seach functiolnality

---
+++ contacts/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth import login
+
+
+class User(AbstractUser):
+    pass
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
+
+    class Meta:
+        unique_together = ('user', 'email')
+    
+    def __str__(self):
+        return f'{self.name} <{self.email}>'
+    
---
+++ contacts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . models import *
+from django.db.models import Q
+
+
+# Create your views here.
+@login_required
+def index(request):
+    contacts = request.user.contacts.all().order_by('-created_at')
+    print(contacts)
+    context = {"contacts": contacts}
+    return render(request, 'contacts.html', context)
+
+
+@login_required
+def search_contact(request):
+    import time
+    time.sleep(2)
+    query = request.GET.get('search', '')
+
+    print(query)
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    )
+    context = {'contacts': contacts}
+
+    return render(request, 'partials/contact-list.html', context)
